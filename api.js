a5fef9dfaabddec9e8c534361989171a96c8b907
Yash Garudkar <yashgarudkar@gmail.com>
2020-05-28 14:53:49 +0530

    Get pizza from pizzas.json

--- api.js
+++ api.js
@@ -1,17 +1,20 @@
 const claudiaapi = require("claudia-api-builder")
+const getPizzas = require("./handlers/get-pizzas").getPizzas
+
 const app = new claudiaapi()
 
+app.get('/', () => 'Welcome to pizzas api!')
 
 app.get('/pizzas', () => {
-    return [
-        'Capricciosa',
-        'Quattro Formaggi',
-        'Napoletana',
-        'Margherita'
-    ]
-});
+    return getPizzas()
+})
+
+app.get('/pizzas/{id}', (request) => {
+    return getPizzas(request.pathParams.id)
+}, {
+    error: 404
+})
 
 module.exports = app
-
 
 //https://tple8gkrke.execute-api.eu-central-1.amazonaws.com/latest/pizzas
